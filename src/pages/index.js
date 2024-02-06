90f29d2ac2a56411cf001b21c388b34fc80b2648
Marcelo Módolo <marcelo.rmodolo@gmail.com>
2024-02-06 15:20:00 -0300

    create launch date

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
-import * as React from "react"
-import { StaticImage } from "gatsby-plugin-image"
-import "../styles/global.css"
+import * as React from "react";
+import { StaticImage } from "gatsby-plugin-image";
+import "../styles/global.css";
 
-import Layout from '../components/layout'
+import Layout from "../components/layout";
 
 const IndexPage = () => {
   return (
@@ -11,39 +11,37 @@ const IndexPage = () => {
         <title>Cloud Coffee</title>
         <meta name="description" content="Bringing Coffee to the Cloud" />
       </header>
-      
-      <main className='main'>
-        
 
-          <h1 className='title'>
-            Welcome to{' '}
-            <code className='title_code'>Cloud Coffee</code>
-          </h1>
-      
+      <main className="main">
+        <h1 className="title">
+          Welcome to <code className="title_code">Cloud Coffee</code>
+        </h1>
 
-          <p className='description'>
-            Join Our Mailing List!
-            <br/>
-            <input type="text"></input>
-            <br/>
-            <button>Submit</button>
-          </p>
+        <h4 className="title_code">Launch Date Announced</h4>
+        <p className="description">
+          Join Our Mailing List!
+          <br />
+          <input type="text"></input>
+          <br />
+          <button>Submit</button>
+        </p>
 
-          <footer className='footer'>
-            <a
-              href="https://acloud.guru"
-            >
-              Powered by{' '}
-              <span className='logo'>
-                <StaticImage src="../images/ACG_Orange_Logo_Horizontal.png" alt="A Cloud Guru logo" width={119} height={16} />
-              </span>
-            </a>
-          </footer>
+        <footer className="footer">
+          <a href="https://acloud.guru">
+            Powered by{" "}
+            <span className="logo">
+              <StaticImage
+                src="../images/ACG_Orange_Logo_Horizontal.png"
+                alt="A Cloud Guru logo"
+                width={119}
+                height={16}
+              />
+            </span>
+          </a>
+        </footer>
       </main>
-
-      
     </Layout>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
